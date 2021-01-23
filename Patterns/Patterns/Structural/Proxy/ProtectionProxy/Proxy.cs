8490d1f8691a1fd572ff8fd5af2267b99c7c1489
YarKa03Coder <yaroslav0302001@gmail.com>
2021-01-23 14:08:05 +0200

    Proxy

---
+++ Patterns/Patterns/Structural/Proxy/ProtectionProxy/Proxy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Patterns.Structural.Proxy.ProtectionProxy
+{
+    public class Proxy : Subject
+    {
+        private readonly Subject _realOperator;
+        private readonly string _accessLevel;
+
+        public Proxy(Subject realOperator, string accessLevel)
+        {
+            _realOperator = realOperator;
+            _accessLevel = accessLevel;
+        }
+
+        public override void Create(string key, string value)
+        {
+            if (_accessLevel.Equals("Owner"))
+            {
+                _realOperator.Create(key, value);
+            }
+            else
+            {
+                throw new UnauthorizedAccessException("Create operation: Access denied.");
+            }
+        }
+
+        public override string Read(string key)
+        {
+            return _realOperator.Read(key);
+        }
+
+        public override bool Update(string key, string value)
+        {
+            if (_accessLevel.Equals("User"))
+            {
+                throw new UnauthorizedAccessException("Update operation: Access denied.");
+            }
+
+            return _realOperator.Update(key, value);
+        }
+
+        public override bool Delete(string key)
+        {
+            if(_accessLevel.Equals("Administrator") || _accessLevel.Equals("Owner"))
+            { 
+                return _realOperator.Delete(key);
+            }
+
+            throw new UnauthorizedAccessException("Delete operation: Access denied.");
+        }
+    }
+}
---
+++ Patterns/Patterns/Structural/Proxy/ProtectionProxy/RealOperator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Patterns.Structural.Proxy.ProtectionProxy
+{
+    public class RealOperator : Subject
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public RealOperator()
+        {
+            _dictionary = new Dictionary<string, string>
+            {
+                {"Pre-created Key","TestValue"}
+            };
+        }
+
+        public override void Create(string key, string value)
+        {
+            _dictionary.Add(key, value);
+        }
+
+        public override string Read(string key)
+        {
+            return _dictionary[key];
+        }
+
+        public override bool Update(string key, string value)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary[key] = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool Delete(string key)
+        {
+            return _dictionary.Remove(key);
+        }
+    }
+}
